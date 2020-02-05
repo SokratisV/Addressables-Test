851934fa818ce3d1e99658b61d3db8e965b832a6
Sokratis Vogiatzakis <sokvogiatzakis@gmail.com>
2020-02-05 21:38:27 +0200

    Tried to make runtime asset reloading, not working.

--- Assets/Scripts/GetAddressable.cs
+++ Assets/Scripts/GetAddressable.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Pipeline.Utilities;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
@@ -9,13 +10,53 @@ public class GetAddressable : MonoBehaviour
 {
     [SerializeField] private List<AssetReference> assetsReferences;
     [SerializeField] private Text downloadSize;
+    
     private long _totalDownloadSize;
+    private readonly Dictionary<AssetReference, AsyncOperationHandle<GameObject>> _asyncOperationHandles =
+        new Dictionary<AssetReference, AsyncOperationHandle<GameObject>>();
 
     public void CheckForUpdates()
     {
-        foreach (var assetReference in assetsReferences)
+        _totalDownloadSize = 0;
+        foreach (var asset in assetsReferences)
         {
-            StartCoroutine(Check(assetReference));
+            StartCoroutine(Check(asset));
+        }
+    }
+
+    public void CleanUp()
+    {
+        foreach (var asset in assetsReferences)
+        {
+            if (_asyncOperationHandles.ContainsKey(asset))
+            {
+                Addressables.ReleaseInstance(_asyncOperationHandles[asset]);
+                _asyncOperationHandles.Remove(asset);
+            }
+        }
+    }
+
+    public void DeepClean()
+    {
+        foreach (var asset in assetsReferences)
+        {
+            if (_asyncOperationHandles.ContainsKey(asset))
+            {
+                Addressables.Release(_asyncOperationHandles[asset]);
+                Addressables.ClearDependencyCacheAsync(asset);
+            }
+        }
+        Caching.ClearCache();
+        downloadSize.text = "0.0";
+        CleanUp();
+    }
+
+    public void InstantiateInstances()
+    {
+        foreach (var asset in assetsReferences)
+        {
+            if (_asyncOperationHandles.ContainsKey(asset)) return;
+            _asyncOperationHandles[asset] = Addressables.InstantiateAsync(asset);
         }
     }
 
@@ -26,27 +67,25 @@ public class GetAddressable : MonoBehaviour
         {
             yield return null;
         }
-
         _totalDownloadSize += op.Result;
 
         // IF updates found, download updates.
         downloadSize.text = BytesToMegabytes(_totalDownloadSize);
+        
         if (op.Result > 0)
         {
             var op1 = Addressables.DownloadDependenciesAsync(assetReference);
             while (!op1.IsDone)
             {
+                print(op1.PercentComplete);
                 yield return null;
             }
         }
-
-        assetReference.InstantiateAsync();
     }
 
     private string BytesToMegabytes(long totalDownloadSize)
     {
-        var value = string.Format("{0:F1}", totalDownloadSize / 1024f / 1024f);
-        print(value);
+        var value = $"{totalDownloadSize / 1024f / 1024f:F1}";
         return value;
     }
 }
