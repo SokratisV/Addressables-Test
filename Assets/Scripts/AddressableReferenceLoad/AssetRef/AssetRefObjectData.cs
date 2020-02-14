1a4c930cbc10d1f54db1e4229ecf7529e3497c64
Sokratis Vogiatzakis <sokvogiatzakis@gmail.com>
2020-02-15 00:24:01 +0200

    More methods to test stuff.

--- Assets/Scripts/AddressableReferenceLoad/AssetRef/AssetRefObjectData.cs
+++ Assets/Scripts/AddressableReferenceLoad/AssetRef/AssetRefObjectData.cs
@@ -1,24 +1,112 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class AssetRefObjectData : MonoBehaviour
 {
-    [SerializeField] private AssetReference _asset;
-    [SerializeField] private List<AssetReference> _references = new List<AssetReference>();
+    [SerializeField] private List<AssetReference> references = new List<AssetReference>();
 
-    [SerializeField] private List<GameObject> _completedObjects = new List<GameObject>();
-    private void Start()
+    private Dictionary<AssetReference, List<GameObject>> _loadedObjects =
+        new Dictionary<AssetReference, List<GameObject>>();
+
+    // [SerializeField] private List<GameObject> instantiatedObjects = new List<GameObject>();
+    // [SerializeField] private List<GameObject> loadedObjects = new List<GameObject>();
+
+    //<Invoked from buttons>
+    public void InstantiateAssets()
     {
-        _references.Add(_asset);
+        StartCoroutine(InstantiateAndWaitUntilComplete());
+    }
 
+    public void LoadAssets()
+    {
         StartCoroutine(LoadAndWaitUntilComplete());
+    }
+
+    public void ReleaseAssets()
+    {
+        // _ReleaseAssets(loadedObjects);
+        _ReleaseAssets(_loadedObjects);
+    }
+
+    public void ReleaseFirstAsset()
+    {
+        // if (loadedObjects.Count <= 0) return;
+        //
+        // ReleaseAsset(loadedObjects[0]);
+        // loadedObjects.RemoveAt(0);
+    }
+
+    public void ReleaseAllInstances()
+    {
+        // _ReleaseAllInstances(instantiatedObjects);
+        _ReleaseAllInstances(_loadedObjects);
+    }
+
+    public void ReleaseFirstInstance()
+    {
+        _ReleaseInstance(_loadedObjects, references[0]);
+        // _loadedObjects[references[0]].RemoveAt(0);
+    }
+
+    public void Instantiate()
+    {
+    }
+    //</Invoked from buttons>
+
+    private static void _ReleaseAllInstances(List<GameObject> gameObjects)
+    {
+        if (gameObjects.Count <= 0) return;
+
+        AssetRefLoader.ReleaseAllInstances(gameObjects);
+        gameObjects.Clear();
+    }
+
+    private static void _ReleaseAllInstances(Dictionary<AssetReference, List<GameObject>> dictionary)
+    {
+        //Assets are being unloaded when none is in use, apparently
+        AssetRefLoader.ReleaseAssetInstancesFromList(dictionary);
+    }
+
+    private static void _ReleaseInstance(GameObject go)
+    {
+        AssetRefLoader.ReleaseAssetInstance(go);
+    }
+
+    private static void _ReleaseInstance(Dictionary<AssetReference, List<GameObject>> dictionary,
+        AssetReference reference)
+    {
+        AssetRefLoader.ReleaseAssetFromList(dictionary, reference);
+    }
+
+    private static void _ReleaseAssets(List<GameObject> gameObjects)
+    {
+        AssetRefLoader.ReleaseAssetsFromList(gameObjects);
+        gameObjects.Clear();
+    }
+
+    private static void _ReleaseAssets(Dictionary<AssetReference, List<GameObject>> dictionary)
+    {
+        AssetRefLoader.ReleaseUnusedAssetsFromList(dictionary);
+    }
+
+    private static void ReleaseAsset(GameObject go)
+    {
+        AssetRefLoader.ReleaseAsset(go);
+    }
+
+    //No need for Coroutine (for now, TODO: Change to normal)
+    private IEnumerator InstantiateAndWaitUntilComplete()
+    {
+        // yield return AssetRefLoader.InstantiateAndAddToList(references, instantiatedObjects);
+        yield return AssetRefLoader.InstantiateFromList(_loadedObjects, references);
     }
 
     private IEnumerator LoadAndWaitUntilComplete()
     {
-        yield return AssetRefLoader.CreateAssetsAddToList(_references, _completedObjects);
+        // yield return AssetRefLoader.LoadAndAddToList(references, loadedObjects);
+        yield return AssetRefLoader.LoadAndAddToList(_loadedObjects, references);
     }
 }
--- Assets/Scripts/Scene1/UpAndDown.cs
+++ Assets/Scripts/Scene1/UpAndDown.cs
@@ -2,8 +2,20 @@
 
 public class UpAndDown : MonoBehaviour
 {
+    private Vector3 _randomPosition;
+    private const float Speed = 1.5f;
+
+    private void Start()
+    {
+        _randomPosition = new Vector3(Random.Range(-3,3),Random.Range(-3,3),Random.Range(-3,3));
+    }
+
     void Update()
     {
-        transform.position = new Vector3(Random.Range(-3,3),Random.Range(-3,3),Random.Range(-3,3));        
+        transform.position = Vector3.MoveTowards(transform.position, _randomPosition, Time.deltaTime * Speed);
+        if (Vector3.Distance(transform.position, _randomPosition) <= .1f)
+        {
+            _randomPosition = new Vector3(Random.Range(-3,3),Random.Range(-3,3),Random.Range(-3,3));
+        }
     }
 }
